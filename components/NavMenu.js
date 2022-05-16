a2c121dee8534b8a80b1d60da7166749ec5b0c40
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2022-05-16 08:35:32 +0200

    creating dropdown

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -12,12 +12,10 @@ import { useEffect, useState } from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { GrFormClose } from "react-icons/gr";
 
-
-
 const NavMenu = () => {
     const [open, setOpen] = useState(false);
 
-    const handleBurger = () =>{
+    const handleBurger = () => {
         setOpen(!open);
     };
 
@@ -29,34 +27,36 @@ const NavMenu = () => {
     //     }
     // });
 
-
     return (
         <>
-            <Navbar bg="light" expand="lg">
+            <Navbar expand="lg">
                 <Container>
                     <Navbar.Brand href="#home">
                         <Image
                             src="/logo-blir-rorlegger.svg"
                             alt="Picture of the author"
-                            width={70}
-                            height={70}
+                            width={110}
+                            height={110}
                         />
                     </Navbar.Brand>
                     <div className="hamburger-dropdown">
-                        <div className= { open ? "close-btn" : "open-btn" }
+                        <div
+                            className={open ? "close-btn" : "open-btn"}
                             onClick={() => setOpen(!open)}
                             aria-controls="nav-menu"
                             aria-expanded={open}
                         >
-                        <HiOutlineMenuAlt3 className="open-icon"/>
-                        <GrFormClose className="close-icon"/>
+                            <HiOutlineMenuAlt3 className="open-icon" />
+                            <GrFormClose className="close-icon" />
                         </div>
                         <Collapse in={open}>
                             <Nav id="nav-menu">
                                 <Nav.Link href="#home">Forsiden</Nav.Link>
                                 <Nav.Link href="#link">Arbeidsliv</Nav.Link>
                                 <Nav.Link href="#link">Kontakt oss</Nav.Link>
-                                <Nav.Link href="#link">Våre ambassadører</Nav.Link>
+                                <Nav.Link href="#link">
+                                    Våre ambassadører
+                                </Nav.Link>
                             </Nav>
                         </Collapse>
                     </div>
