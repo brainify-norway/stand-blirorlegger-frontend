5d9069b99fd04aa7ff184a065529db3d20058225
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2022-05-31 13:09:51 +0200

    videos fixed and added

--- components/videoCard.js
+++ components/videoCard.js
@@ -1,33 +1,34 @@
 import { useState, useEffect } from "react";
 import QuestionModal from "../components/questionModal";
 
-
 export default function VideoCard({ item }) {
     const [open, setOpen] = useState(false);
 
-    if(!item.acf.featuredVideo){
-        console.log("test")
+    if (!item.acf.featuredVideo) {
+        console.log("test");
     }
     console.log(item.acf.featuredVideo);
-
 
     useEffect(() => {});
 
     return (
         <>
             <div key={item.id} className="video-card">
-                <video
-                    style={{
-                        width: "100%",
-                        height: "500px"
-                    }}
-                    autoPlay
-                    loop
-                    muted
-                >
-                   { item.acf.featuredVideo && <source src={item.acf.featuredVideo.mediaItemUrl} />}
-
-                </video>
+                <div className="video-box">
+                    <video
+                        style={{
+                            width: "100%",
+                            height: "500px"
+                        }}
+                        autoPlay
+                        loop
+                        muted
+                    >
+                        {item.acf.featuredVideo && (
+                            <source src={item.acf.featuredVideo.mediaItemUrl} />
+                        )}
+                    </video>
+                </div>
                 <div className="openModal" onClick={() => setOpen(true)}>
                     <span className="text">Still meg et spørsmål</span>
                 </div>
