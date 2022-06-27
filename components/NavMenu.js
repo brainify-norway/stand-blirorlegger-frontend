496d3d512b0f8b967868b1557615d0e33ce72b21
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2022-06-27 11:03:21 +0200

    fixed navburger

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -9,8 +9,6 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import { GrFormClose } from "react-icons/gr";
 
 const NavMenu = () => {
     const [open, setOpen] = useState(false);
@@ -28,45 +26,42 @@ const NavMenu = () => {
                         />
                     </Navbar.Brand>
 
-                    <div className="hamburger-dropdown">
-                        <div
-                            className={open ? "close-btn" : "open-btn"}
-                            onClick={() => setOpen(!open)}
-                            aria-controls="nav-menu"
-                            aria-expanded={open}
-                        >
-                            <HiOutlineMenuAlt3 className="open-icon" />
-                            <GrFormClose className="close-icon" />
-                        </div>
-                        <Collapse in={open}>
-                            <Nav id="nav-menu">
-                                <Link
-                                    className="menu-link"
-                                    href="https://blirorlegger.no/"
-                                >
-                                    Forsiden
-                                </Link>
-                                <Link
-                                    className="menu-link"
-                                    href="https://blirorlegger.no/arbeidsliv/"
-                                >
-                                    Arbeidsliv
-                                </Link>
-                                <Link
-                                    className="menu-link"
-                                    href="https://blirorlegger.no/kontakt/"
-                                >
-                                    Kontakt oss
-                                </Link>
-                                <Link
-                                    className="menu-link"
-                                    href="https://blirorlegger.no/rorambassadorer/"
-                                >
-                                    Våre ambassadører
-                                </Link>
-                            </Nav>
-                        </Collapse>
-                    </div>
+                    <div
+                        className={
+                            open
+                                ? "hamburger-dropdown close"
+                                : "hamburger-dropdown  open"
+                        }
+                        onClick={() => setOpen(!open)}
+                    ></div>
+                    <Collapse className={open ? "show" : "hide"}>
+                        <Nav id="nav-menu">
+                            <Link
+                                className="menu-link"
+                                href="https://blirorlegger.no/"
+                            >
+                                Forsiden
+                            </Link>
+                            <Link
+                                className="menu-link"
+                                href="https://blirorlegger.no/arbeidsliv/"
+                            >
+                                Arbeidsliv
+                            </Link>
+                            <Link
+                                className="menu-link"
+                                href="https://blirorlegger.no/kontakt/"
+                            >
+                                Kontakt oss
+                            </Link>
+                            <Link
+                                className="menu-link"
+                                href="https://blirorlegger.no/rorambassadorer/"
+                            >
+                                Våre ambassadører
+                            </Link>
+                        </Nav>
+                    </Collapse>
                 </Container>
             </Navbar>
         </>
